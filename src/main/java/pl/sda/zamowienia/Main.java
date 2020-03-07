3c3ccb890c6ec502526d6bc60139b5e83b1bf805
Daniel <kapron.daniel@gmail.com>
2020-03-07 14:41:50 +0100

    Zadanko zamowienia - wyjatki

---
+++ src/main/java/pl/sda/zamowienia/Main.java
@@ -0,0 +1,11 @@
+package pl.sda.zamowienia;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Typ wyliczeniowy - ENum
+
+    }
+
+}
---
+++ src/main/java/pl/sda/zamowienia/Product.java
@@ -0,0 +1,7 @@
+package pl.sda.zamowienia;
+
+public class Product {
+
+
+
+}
